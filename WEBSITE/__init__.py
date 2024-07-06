8acc40c84e9d6675a18878ce5b3e642c1929ec01
Marcial Bernal <marcialb9328@gmail.com>
2024-07-05 19:04:46 -0700

    modified:   WEBSITE/__init__.py

--- WEBSITE/__init__.py
+++ WEBSITE/__init__.py
@@ -4,8 +4,8 @@ from config import ProductionConfig, DevelopmentConfig
 app = Flask(__name__)
 
 if app.config['ENV'] == "production":
-    app.config.from_object("config.ProductionConfig")
+    app.config.from_object(ProductionConfig)
 else:
-    app.config.from_object("config.DevelopmentConfig")
-
+    app.config.from_object(DevelopmentConfig)
+    
 from WEBSITE import routes
