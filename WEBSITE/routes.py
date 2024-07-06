9c08101dfd608168cad9f94458be6d16a667770f
Marcial Bernal <marcialb9328@gmail.com>
2024-07-06 16:29:45 -0700

    ADD METHODS ON ROUTES

--- WEBSITE/routes.py
+++ WEBSITE/routes.py
@@ -5,11 +5,11 @@ from flask import render_template, send_from_directory
 
 ####################    ####################  
 
-@app.route("/")
+@app.route("/", methods=['GET'])
 def home():
     return render_template("index.html")
 
-@app.route('/download_cv')
+@app.route('/download_cv', methods=['GET'])
 def download_cv():
     return send_from_directory(directory='static', filename='CV.pdf')
 
