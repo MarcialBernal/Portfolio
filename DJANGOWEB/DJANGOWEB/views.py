8e55399613a94fa5cfefb8307d5347623424525c
Marcial Bernal <marcialb9328@gmail.com>
2024-11-17 22:43:29 -0700

    django learning

--- DJANGOWEB/DJANGOWEB/views.py
+++ DJANGOWEB/DJANGOWEB/views.py
@@ -1,6 +1,7 @@
 from django.http import HttpResponse
 import datetime
 from django.template import Template, Context, loader
+from django.shortcuts import render
 
 def home(request):
     
@@ -10,13 +11,14 @@ def home(request):
     #home_template = open("C:/Users/Marcial/Desktop/DEV/PORTFOLIO/DJANGOWEB/DJANGOWEB/templates/home.html")
     #plt = Template(home_template.read())
     #home_template.close()
-    home_template = loader.get_template("home.html")
+    #home_template = loader.get_template("home.html")
     #ctx = Context({"today_date":today_date, "colors":colors})
     #home_render = plt.render(ctx)
     #home_render = home_template.render(ctx)
-    home_render = home_template.render({"today_date":today_date, "colors":colors})
+    #home_render = home_template.render({"today_date":today_date, "colors":colors})
     
-    return HttpResponse(home_render)
+    #return HttpResponse(home_render)
+    return render(request, "home.html", {"today_date":today_date, "colors":colors} )
 
 def bye(request):
     return HttpResponse("Good Bye")
