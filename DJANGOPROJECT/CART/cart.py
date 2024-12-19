09c3098f168bfce5492bdfce7d17a834fd63236b
Marcial Bernal <marcialb9328@gmail.com>
2024-12-18 19:11:40 -0700

    django lessons cart created

--- DJANGOPROJECT/CART/cart.py
+++ DJANGOPROJECT/CART/cart.py
@@ -1,2 +1,51 @@
 class Cart:
-    
+    def __init__(self, request):
+        self.request = request
+        self.session = request.session
+        cart = self.session.get("cart")
+        if not cart:
+            cart = self.session["cart"] = {}
+            
+        else:
+            self.cart = cart
+    ###
+    def add(self, item):
+        if (str(item.id) not in self.cart.keys()):
+            self.cart[item.id] = {
+                "item_id":item.id, 
+                "name":item.name, 
+                "price":str(item.price),
+                "quantity": 1,
+                "image":item.image.url
+                }
+            
+        else:
+            for key, value in self.cart.items():
+                if key == str(item.id):
+                    value["quantity"] = value["quantity"]+1
+                    break
+                
+        self.save()
+    ###    
+    def save(self):
+        self.session["cart"] = self.cart
+        self.session.modified = True
+    ###    
+    def delete(self, item):
+        item.id = str(item.id)
+        if item.id in self.cart:
+            del self.cart[item.id]
+            self.save()
+    ###        
+    def substract(self, item):
+        for key, value in self.cart.items():
+                if key == str(item.id):
+                    value["quantity"] = value["quantity"]-1
+                    if value["quantity"]<1:
+                        self.delete(item)
+                    break
+        self.save()
+    ###    
+    def clean(self):
+        self.session["cart"] = {}
+        self.session.modified = True
--- DJANGOPROJECT/CART/views.py
+++ DJANGOPROJECT/CART/views.py
@@ -1,3 +1,33 @@
-from django.shortcuts import render
+from django.shortcuts import render, redirect
+from .cart import Cart
+from SHOP.models import Items
+
 
 # Create your views here.
+
+def add_item(request, item_id):
+    cart = Cart(request)
+    item = Items.objects.get(id=item_id)
+    cart.add(item=item)
+    
+    return redirect("shop")
+###
+def delete_item(request, item_id):
+    cart = Cart(request)
+    item = Items.objects.get(id=item_id)
+    cart.delete(item=item)
+    
+    return redirect("shop")
+###
+def substract_item(request, item_id):
+    cart = Cart(request)
+    item = Items.objects.get(id=item_id)
+    cart.substract(item=item)
+    
+    return redirect("shop")
+###
+def clean_cart(request, item_id):
+    cart = Cart(request)
+    cart.clean()
+    
+    return redirect("shop")
